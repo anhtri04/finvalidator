43cf71a8e73c3d1a9e6de8fea6e29cc9e282147c
anhtri04 <mastercuberskill@gmail.com>
2025-10-10 22:49:38 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# FinValidator
+
+A lightweight Java validation library for banking and fintech data.
+
+## Features
+- Account number, citizen ID, phone, email validation
+- Luhn-based card number check
+- Password strength and date format validation
+
+## Installation
+```bash
+mvn clean install
---
+++ src/main/java/com/hydrangea/finvalidator/validators/PhoneNumberValidator.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.hydrangea.finvalidator.validators;
+
+/**
+ *
+ * @author Anh Tri
+ */
+public class PhoneNumberValidator {
+
+}
